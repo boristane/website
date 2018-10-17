eebc0f4c27f052238b8dbe00105b9dcf0471e932
boristane <boris.tane@gmail.com>
2018-10-17 01:16:58 +0100

    Implements greetings

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+    "extends": "airbnb-base",
+    "env": {
+        "browser": true,
+        "node": true,
+        "jasmine": true
+    },
+    "rules": {
+        "indent": [
+            "error",
+            4,
+        ],
+    }
+};
---
+++ js/index.js
@@ -0,0 +1,62 @@
+function randomiseStringInDOMElt(elt) {
+    const chars = ['$', '%', '#', '@', '&', '(', ')', ',', '=', '*', '/'];
+    const getRandomInt = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
+    const letters = elt.textContent.split('');
+    let displayString = '';
+
+    // Randomising times in milliseconds
+    const randomisingTime = 40;
+    const randomisingTime2 = 60;
+    const randomisingTime3 = 30;
+
+    letters.forEach((char, index) => {
+        const delay1 = index * randomisingTime;
+        setTimeout(() => {
+            displayString += Math.random() < 0.5 ? chars[getRandomInt(0, chars.length - 1)] : char;
+            elt.textContent = displayString;
+        }, delay1);
+
+        const delay2 = letters.length * randomisingTime + index * randomisingTime2;
+        setTimeout(() => {
+            const arr = displayString.split('');
+            arr[index] = char;
+            elt.textContent = arr.join('');
+        }, delay2);
+
+        const delay3 = letters.length * randomisingTime + letters.length
+        * randomisingTime2 + index * randomisingTime3;
+        setTimeout(() => {
+            const arr = displayString.split('');
+            arr[index] = char;
+            displayString = arr.join('');
+            elt.textContent = displayString;
+        }, delay3);
+    });
+}
+
+const helloTranslations = ['Bonjour', 'Hola', 'Hallo', 'Namaste', 'Salaam',
+    'Konnichiwa', 'Merhaba', 'Marhaba', 'Ni Hau', 'Hoi', 'Shalom', 'Sawubona'];
+
+function $(s) {
+    return document.getElementById(s) || document.querySelector(s);
+}
+
+function $s(s) {
+    return document.querySelectorAll(s);
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    function hello() {
+        let workingHellos = [...helloTranslations];
+        const interval = 2000;
+        setInterval(() => {
+            const randomIndex = Math.floor(Math.random() * workingHellos.length);
+            $('hello').textContent = `${workingHellos[randomIndex]} !`;
+            randomiseStringInDOMElt($('hello'));
+            workingHellos = workingHellos.filter(elt => elt !== workingHellos[randomIndex]);
+            if (workingHellos.length === 0) workingHellos = [...helloTranslations, 'Hello'];
+        }, interval);
+    }
+
+    hello();
+});
