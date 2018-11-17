88084f08dd9e99a526582e282fd9791f22b3785b
Boris Tane <boris.tane@gmail.com>
2018-11-17 20:35:03 +0000

    Integrate Vue.js on Projects page (#1)
    * Add Vue on the projets page
    
    * Connect to REST API
    
    * Remove images

---
+++ js/projects.js
@@ -0,0 +1,15 @@
+/* eslint-disable func-names */
+/* eslint-disable object-shorthand */
+const app = new Vue({
+    el: '#app',
+    data: {
+        projects: '',
+    },
+    created: function () {
+        const url = 'https://boristane-blog-api.herokuapp.com/projects';
+        fetch(url).then(res => res.json())
+            .then((data) => {
+                this.projects = data.projects;
+            });
+    },
+});
