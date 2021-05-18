ed473697aa21c543b3d97a360c4af30354481bce
Boris Tane <boris.tane@gmail.com>
2021-05-18 17:21:38 +0100

    thinsg

--- js/index.js
+++ js/index.js
@@ -67,7 +67,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
     hello();
 
-    const strings = ["blog", "stuff", "!feature", "Twitter"];
+    const strings = ["blog", "stuff", "!feature", "myself", "Twitter"];
     const links = document.querySelectorAll(".details a");
     links.forEach((link, index) => {
         link.textContent = strings[index];
