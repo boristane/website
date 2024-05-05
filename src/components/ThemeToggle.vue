d26408fa9912a2fad9b2c46275c6d52cf0e596f2
Boris Tane <boris.tane@gmail.com>
2024-05-05 12:39:08 +0100

    comment out the dark mode because it flashes and I can't figure how to solve this

--- src/components/ThemeToggle.vue
+++ src/components/ThemeToggle.vue
@@ -1,10 +1,10 @@
 <script lang="ts" setup>
 import { useDark, useToggle } from '@vueuse/core'
 
-const isDark = useDark()
-const toggleDark = useToggle(isDark)
+// const isDark = useDark()
+// const toggleDark = useToggle(isDark)
 </script>
 
 <template>
-  <i nav-link dark:i-ri-moon-line i-ri-sun-line @click="toggleDark()" />
+  <!-- <i nav-link dark:i-ri-moon-line i-ri-sun-line @click="toggleDark()" /> -->
 </template>
