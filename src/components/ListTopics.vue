9c9b17a3aaed626cc2d69160100ed8b1dc30ff13
Boris Tane <boris.tane@gmail.com>
2024-05-10 17:13:58 +0100

    add projects

---
+++ src/components/ListTopics.vue
@@ -0,0 +1,25 @@
+<script lang="ts" setup>
+
+
+withDefaults(defineProps<{
+  topics: string[]
+}>(), {
+  topics: () => [],
+})
+
+
+
+
+
+</script>
+
+<template>
+  <ul v-if="topics && topics.length" class="flex items-center gap-1 text-main text-gray-400">[
+    <li v-for="(topic, index) in topics " :key="topic">
+      <span lh-normal class="bg-gray-100 px-2 py-[1px] text-sky-600 rounded">
+        {{ topic }}
+      </span>
+    </li>
+    ]
+  </ul>
+</template>
